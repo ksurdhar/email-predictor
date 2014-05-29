93b3a90b46794104fac6b9342255e432f1a9dd21
Kiran Surdhar <ksurdhar1@gmail.com>
2014-05-29 14:04:35 -0400

    First commit, added comments on how to approach problem.

---
+++ email_predictor.rb
@@ -0,0 +1,8 @@
+#given a name and domain, predict an email address
+#four patterns possible: first.last, f.last, first.l, f.l
+
+#create a class which stores a hash of companies to patterns
+#write two methods, produce_email(name, company) and update_pattern(company, pattern)
+
+#patterns can also be stored and be represented by numbers
+#produce_email will return either an array of four addresses, one address, or nothing
